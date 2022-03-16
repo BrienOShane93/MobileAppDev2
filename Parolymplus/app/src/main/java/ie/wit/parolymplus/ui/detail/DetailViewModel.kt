c7647469487274de65d3884484daf3d6c5ac894a
Shane O'Brien <brienoshane93@gmail.com>
2022-03-16 18:57:25 +0000

    init setup and refactoring

---
+++ Parolymplus/app/src/main/java/ie/wit/parolymplus/ui/detail/DetailViewModel.kt
@@ -0,0 +1,13 @@
+package ie.wit.parolymplus.ui.detail
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is detail Fragment"
+    }
+    val text: LiveData<String> = _text
+}
